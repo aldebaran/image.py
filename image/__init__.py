a27aef50286aaaee4aa9e322234db3fe0e368fd4
sambrose <sambrose@aldebaran.com>
2017-08-23 11:01:35 +0200

    Fix bad VERSION import

--- image/__init__.py
+++ image/__init__.py
@@ -6,6 +6,6 @@ import os as _os
 __VERSION__=open(
     _os.path.join(
         _os.path.dirname(_os.path.realpath(__file__)),
-        "image/VERSION"
+        "VERSION"
     )
 ).read().split()[0]
