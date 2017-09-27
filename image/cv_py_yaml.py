c883210e8271e1040b6711d42bb62907716e6584
sambrose <sambrose@aldebaran.com>
2017-09-27 12:23:14 +0200

    Add License

--- image/cv_py_yaml.py
+++ image/cv_py_yaml.py
@@ -1,3 +1,31 @@
+# Copyright (c) 2017, Softbank Robotics Europe
+# All rights reserved.
+
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+
+# * Neither the name of the copyright holder nor the names of its
+#   contributors may be used to endorse or promote products derived from
+#   this software without specific prior written permission.
+
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 # Standard libraries
 import warnings
 
@@ -38,6 +66,7 @@ def writeCvYamlFile(cv_yaml_file, content):
 
 # A yaml constructor is for loading from a yaml node.
 # This is taken from @misha 's answer: http://stackoverflow.com/a/15942429
+# to the following topic: https://stackoverflow.com/questions/11141336/filestorage-for-opencv-python-api
 # and modified
 def _opencvNDMatrixConstructor(loader, node):
 	mapping = loader.construct_mapping(node, deep=True)
